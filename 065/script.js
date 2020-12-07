01d95ab5183a0503b17fe2bb5e48d859cb75732b
Dima <melnychuk.ss.1024@gmail.com>
2020-12-07 23:50:19 +0200

    lessons 065

---
+++ 065/script.js
@@ -0,0 +1,33 @@
+// new ReqExp('pattern', 'flags');
+// /pattern/flags
+
+// const ans = prompt('Write only figures');
+
+// const req = /\d/g;
+
+// console.log(ans.match(req));
+
+const str = 'My name is R2R2';
+console.log(str.match(/\D/ig));
+
+// \d search figures
+// \w search all words and letters
+// \s search all probels __________
+
+// \D not figures ... (caps D)
+// \W now words or letters ... (caps W)
+
+
+// i register 
+// g
+// m
+
+// console.log(ans.search(req));
+// console.log(ans.match(req));
+
+// const pass = prompt('your password');
+
+// console.log(pass.replace(/./g, "*")); 
+// . all elements, g - global all caps, "*" - element what we replace text
+
+// console.log('12-34-56'.replace(/-/g, ':'));
